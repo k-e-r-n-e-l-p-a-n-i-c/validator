82de09bdb722a867c8669003c7e2e10958ba43fd
Arun prasad Krishnaraj <34764@Hexaware.com>
2020-12-05 12:06:07 +0530

    updated

---
+++ app/controllers/app.go
@@ -0,0 +1,20 @@
+package controllers
+
+import (
+	"fmt"
+
+	"github.com/revel/revel"
+	"k8s.io/api/admission/v1beta1"
+)
+
+type App struct {
+	*revel.Controller
+}
+
+func (c App) Validate() revel.Result {
+	var request v1beta1.AdmissionReview
+	c.BindJSON(request)
+
+	fmt.Println(request)
+	return c.RenderText("ok")
+}
