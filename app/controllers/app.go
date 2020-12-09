a4e29a1c4ed80a867f43228edd343495beed2648
Arun prasad Krishnaraj <34764@Hexaware.com>
2020-12-09 14:09:51 +0530

    updated response object

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,10 +1,12 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/revel/revel"
 	"k8s.io/api/admission/v1beta1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -14,17 +16,40 @@ type App struct {
 
 func (c App) Validate() revel.Result {
 	var request v1beta1.AdmissionReview
+	var obj v1.Pod
 	c.Params.BindJSON(&request)
 
-	fmt.Println(request)
+	rawObject := request.Request.Object.Raw
 
-	arResponse := v1beta1.AdmissionReview{
-		Response: &v1beta1.AdmissionResponse{
-			Allowed: false,
-			Result: &metav1.Status{
-				Message: "Keep calm and not add more crap in the cluster!",
+	err := json.Unmarshal(rawObject, &obj)
+	if err != nil {
+		fmt.Println("Error occurred while deserializing request")
+		fmt.Println("Policy check will be skipped")
+		response := v1beta1.AdmissionReview{
+			Response: &v1beta1.AdmissionResponse{
+				Allowed: true,
 			},
+		}
+		return c.RenderJSON(response)
+	}
+
+	if obj.Namespace == "default" {
+		response := v1beta1.AdmissionReview{
+			Response: &v1beta1.AdmissionResponse{
+				Allowed: false,
+				Result: &metav1.Status{
+					Message: "Deployments in 'default' namespace is restricted by cluster admin!",
+				},
+			},
+		}
+		return c.RenderJSON(response)
+	}
+
+	fmt.Println("Pod " + obj.Name + " was allowed by validator")
+	response := v1beta1.AdmissionReview{
+		Response: &v1beta1.AdmissionResponse{
+			Allowed: true,
 		},
 	}
-	return c.RenderJSON(arResponse)
+	return c.RenderJSON(response)
 }
